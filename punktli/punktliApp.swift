0e325767a1fc8adeb94e65f12dd178893d7f8858
Dominik Hofer <hfrdmnk@gmail.com>
2022-08-29 21:53:52 +0200

    Initial Commit

---
+++ punktli/punktliApp.swift
@@ -0,0 +1,17 @@
+//
+//  punktliApp.swift
+//  punktli
+//
+//  Created by Dominik Hofer on 29.08.22.
+//
+
+import SwiftUI
+
+@main
+struct punktliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
