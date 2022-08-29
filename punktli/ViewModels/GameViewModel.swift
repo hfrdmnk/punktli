a08a9da650d14ff2673c091d83238843a3e6a318
Dominik Hofer <hfrdmnk@gmail.com>
2022-08-29 22:14:15 +0200

    Fixed data model

--- punktli/ViewModels/GameViewModel.swift
+++ punktli/ViewModels/GameViewModel.swift
@@ -17,7 +17,13 @@ import Foundation
  */
 
 class GameViewModel: ObservableObject {
-    @Published var teams: [Team] = [] {
+    @Published var team1: Team = Team(name: "Dragons", emoji: "🐉") {
+        didSet {
+            saveData()
+        }
+    }
+    
+    @Published var team2: Team = Team(name: "Jokers", emoji: "🃏") {
         didSet {
             saveData()
         }
@@ -28,7 +34,8 @@ class GameViewModel: ObservableObject {
             saveData()
         }
     }
-    let teamsKey: String = "teams"
+    let team1Key: String = "team1"
+    let team2Key: String = "team2"
     let roundsKey: String = "rounds"
     
     init() {
@@ -37,8 +44,13 @@ class GameViewModel: ObservableObject {
     
     func getData() {
         guard
-            let teams = UserDefaults.standard.data(forKey: teamsKey),
-            let savedTeams = try? JSONDecoder().decode([Team].self, from: teams)
+            let team1 = UserDefaults.standard.data(forKey: team1Key),
+            let savedTeam1 = try? JSONDecoder().decode(Team.self, from: team1)
+        else { return }
+        
+        guard
+            let team2 = UserDefaults.standard.data(forKey: team2Key),
+            let savedTeam2 = try? JSONDecoder().decode(Team.self, from: team2)
         else { return }
         
         guard
@@ -46,13 +58,18 @@ class GameViewModel: ObservableObject {
             let savedRounds = try? JSONDecoder().decode([Round].self, from: rounds)
         else { return }
         
-        self.teams = savedTeams
+        self.team1 = savedTeam1
+        self.team2 = savedTeam2
         self.rounds = savedRounds
     }
     
     func saveData() {
-        if let encodedTeams = try? JSONEncoder().encode(teams) {
-            UserDefaults.standard.set(encodedTeams, forKey: teamsKey)
+        if let encodedTeam1 = try? JSONEncoder().encode(team1) {
+            UserDefaults.standard.set(encodedTeam1, forKey: team1Key)
+        }
+        
+        if let encodedTeam2 = try? JSONEncoder().encode(team2) {
+            UserDefaults.standard.set(encodedTeam2, forKey: team2Key)
         }
         
         if let encodedRounds = try? JSONEncoder().encode(rounds) {
